2d561fdd4f9d41d7acb2b44a2ba8020aca96dff2
fkryadr <70735903+fkryadr@users.noreply.github.com>
2020-12-13 18:44:30 -0800

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Fikrii" },
+    { quote: "I love you.", author: "Fikii" },
+    { quote: "Aku sayang kamu!", author: "Kray" },
+    { quote: "Ian sayang Ava ♥", author: "gapunya siapa siapa" },
+    { quote: "Love chuuu", author: "fifikk" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["boo", "myboo", "Sayang", "Babe", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
